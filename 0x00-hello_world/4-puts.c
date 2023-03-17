a166b61259af5fd804179e7643f0d270101c5bb1
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-17 06:32:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-	puts("Programming is like building a multilingual puzzle,\n");
-	return 0;
+/**
+ * main - A program prints a line with puts func
+ * return 0 success
+ */
+int main(void) 
+{
+puts("Programming is like building a multilingual puzzle,\n");
+return 0;
 }
