b0cb80a82ccc54124ab62a5487a5ec60bfe9d30c
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-18 02:52:34 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 	printf("The size of char: %lu bytes.\n", sizeof(char));
 	printf("The size of int: %lu bytes.\n", sizeof(int));
-	printf("The size of long: %lu bytes.\n", sizeof(long));
-	printf("The size of long long: %lu bytes.\n", sizeof(long long));
+	printf("The size of long: %lu bytes.\n", sizeof(long int));
+	printf("The size of long long: %lu bytes.\n", sizeof(long long int));
 	printf("The size of float: %lu bytes.\n", sizeof(float));
 	return (0);
 }
