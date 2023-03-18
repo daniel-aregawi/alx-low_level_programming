e29a882af7a6d0a2d6d0f2bdeef18b39e7189bf3
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-18 01:58:41 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
-/* main -a program that shows with proper grammar, but the outcome is a piece of art, */
+#include <stdio.h>
+/**
+*main - A program that prints a line using the printf function
+*Return: 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
