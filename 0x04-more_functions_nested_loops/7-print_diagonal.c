95c00e9ad1608daab9db2414566e4dc59b1d7750
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-24 11:48:00 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@ void print_diagonal(int n)
 	if (n <= 0)
 		_putchar('\n');
 	return;
-	for(int i = 1; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= i; j++)
 			if (j == 1)
 				_putchar('\\');
