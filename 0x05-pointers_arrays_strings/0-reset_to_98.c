9e0c5d34d380e33fcf4ea6388b1bd6476ccb4a9a
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-28 07:10:55 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 /**
-* reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
-* @n: pointer to an int
-*
-* Return: void
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * @n: pointer to an int
+ *
+ * Return: void
 */
 void reset_to_98(int *n)
 {
