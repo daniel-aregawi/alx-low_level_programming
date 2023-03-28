d97350cb9f7686ac316a6c065f9ece28316182f4
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-28 07:36:28 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to a char
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+	
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
