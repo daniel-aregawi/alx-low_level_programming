4547e227a9fda7b77df5389d96d321c93ed64dea
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-28 10:17:16 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,32 @@
 #include "holberton.h"
 /**
- *  * _atoi - Converts a string to an integer
- *   * @s: The string to be converted
- *    *
- *     * Return: The integer value of the converted string
- *      */
+* _atoi - Converts a string to an integer
+* @s: The string to be converted
+* Return: The integer value of the converted string
+*/
 int _atoi(char *s)
 {
-	    int sign = 1;
-	        int res = 0;
-		    int i = 0;
+	int sign = 1;
+	int res = 0;
+	int i = 0;
 
-		        while (s[i] == ' ')
-				        i++;
-
-			    if (s[i] == '-')
-				            sign = -1, i++;
-			        else if (s[i] == '+')
-					        i++;
-
-				    while (s[i] >= '0' && s[i] <= '9')
-					        {
-							        if (res > 2147483647 / 10 || (res == 2147483647 / 10 && s[i] - '0' > 7))
-									        {
-											            if (sign == 1)
-													                    return (2147483647);
-												                else
-															                return (-2147483648);
-														        }
-								        res = res * 10 + s[i] - '0';
-									        i++;
-										    }
-
-				        return (sign * res);
+	while (s[i] == ' ')
+		i++;
+	if (s[i] == '-')
+		sign = -1, i++;
+	else if (s[i] == '+')
+		i++;
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (res > 2147483647 / 10 || (res == 2147483647 / 10 && s[i] - '0' > 7))
+		{
+			if (sign == 1)
+				return (2147483647);
+			else
+				return (-2147483648);
+		}
+		res = res * 10 + s[i] - '0';
+		i++;
+	}
+	return (sign * res);
 }
