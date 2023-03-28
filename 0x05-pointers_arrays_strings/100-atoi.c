e19a60ad60cf267d15e43296cb2e2df8520381db
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-28 10:27:27 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,32 +1,40 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
-* _atoi - Converts a string to an integer
-* @s: The string to be converted
-* Return: The integer value of the converted string
-*/
+ *  * _atoi - Convert a string to an integer.
+ *   * @s: The pointer to convert
+ *    *
+ *     * Return: A integer
+ *      */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int res = 0;
-	int i = 0;
+		int c = 0;
+			unsigned int ni = 0;
+				int min = 1;
+					int isi = 0;
 
-	while (s[i] == ' ')
-		i++;
-	if (s[i] == '-')
-		sign = -1, i++;
-	else if (s[i] == '+')
-		i++;
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		if (res > 2147483647 / 10 || (res == 2147483647 / 10 && s[i] - '0' > 7))
-		{
-			if (sign == 1)
-				return (2147483647);
-			else
-				return (-2147483648);
-		}
-		res = res * 10 + s[i] - '0';
-		i++;
-	}
-	return (sign * res);
+						while (s[c])
+								{
+											if (s[c] == 45)
+														{
+																		min *= -1;
+																				}
+
+													while (s[c] >= 48 && s[c] <= 57)
+																{
+																				isi = 1;
+																							ni = (ni * 10) + (s[c] - '0');
+																										c++;
+																												}
+
+															if (isi == 1)
+																		{
+																						break;
+																								}
+
+																	c++;
+																		}
+
+							ni *= min;
+								return (ni);
 }
