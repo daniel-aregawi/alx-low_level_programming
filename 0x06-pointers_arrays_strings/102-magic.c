ddfa9ed470a57327c627e5787480b9a08294c6bb
daniel-aregawi <danielaregawi50@gmail.com>
2023-03-30 10:22:22 -0700

    102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,22 +6,10 @@
 */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+	int a[5] = {0, 1, 2, 3, 4}; /* Initialize array with values */
+	int *p = &a[2]; /* Declare pointer and set it to third element of array */
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	*(p + 1) = 98; /* Assign value of 98 to next element of p */
+	print_array(a, 5); /* Print entire array */
 	return (0);
 }
