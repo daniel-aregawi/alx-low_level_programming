66fd9d742d6415843e129175783ff22440fa893f
daniel-aregawi <danielaregawi50@gmail.com>
2023-04-11 07:08:52 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,9 +9,8 @@
 char *argstostr(int ac, char **av)
 {
 	char *new_string = NULL;
-	int k = 0, i = ac = 0, j, sum = 0, temp = 0;
+	int k = 0, i = ac, j, sum = 0, temp = 0;
 
-	av = NULL;
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	while (ac--)
