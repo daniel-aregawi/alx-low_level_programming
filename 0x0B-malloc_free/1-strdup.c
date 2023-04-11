3ce9c0a4d4dc2420e86b1a0fda25eb2014022b58
daniel-aregawi <danielaregawi50@gmail.com>
2023-04-11 06:34:31 -0700

    1-4

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,19 +11,19 @@
 char *_strdup(char *str)
 {
 	char *cpy;
-	int index, len;
+	int index, len = 0;
 
 	if (str == NULL)
 		return (NULL);
 	for (index = 0; str[index]; index++)
 		len++;
-		cpy = malloc(sizeof(char) * (len + 1));
-		if (cpy == NULL)
-			return (NULL);
-		for (index = 0; str[index]; index++)
+	cpy = malloc(sizeof(char) * (len + 1));
+	if (cpy == NULL)
+		return (NULL);
+	for (index = 0; str[index]; index++)
 	{
 		cpy[index] = str[index];
 	}
-		cpy[len] = '\0';
-		return (cpy);
+	cpy[len] = '\0';
+	return (cpy);
 }
